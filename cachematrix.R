06b5787c8611e10e4bd8d8547726d12de103a80c
pdinakars <paul.dinakaran@clarivate.com>
2020-07-04 15:14:49 -0400

    Added code to create a Cache matrix and retrieve function from cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverse a given array and store it in a cache and
+## if on executing find the inverse already present
+## retrieve it from cache rather than performing the inverse
+## operation
 
-## Write a short comment describing this function
+## makeCacheMatrix function has list of functions to 
+## 1.set matrix 2. get matrix 3. set the inverse of matrix
+## 4. get inverse of matrix
+## Inverse of matrix is performed by ginv function hence used 
+## MASS package
+
+library('MASS')
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(ginv) m <<- ginv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks whether the matrix has been inverted already
+## if done then provides it from cache rather then recalcualting.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- ginv(data)
+  x$setinv(m)
+  m
 }
